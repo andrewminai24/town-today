f7dfbe50773d13d8abc78c46170aaf391552e07e
Richard Prasad <richarddprasad@gmail.com>
2018-09-14 23:38:01 -0700

    Added ScrollView, seems to work

--- town_today_client/view/MasterListComponent.js
+++ town_today_client/view/MasterListComponent.js
@@ -2,8 +2,9 @@
 
 // A container for a set of CategoryList components
 
+// TODO: Cleanup unused imports
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, SectionList } from 'react-native';
+import { StyleSheet, View, Text, SectionList, ScrollView } from 'react-native';
 import CategoryList from './CategoryListComponent';
 import { ITEMS } from '../shared/MockItems';
 
@@ -20,11 +21,16 @@ class MasterList extends Component {
         // TODO: Implement SectionList
         // TODO: Use iterator and get rid of hardcoded values
         return (
-            <View>
+            // <View>
+            //     <CategoryList categoryTitle='Movies' items={this.state.items} />
+            //     <CategoryList categoryTitle='Sports' items={this.state.items} />
+            //     <CategoryList categoryTitle='Music' items={this.state.items} />
+            // </View>
+            <ScrollView>
                 <CategoryList categoryTitle='Movies' items={this.state.items} />
                 <CategoryList categoryTitle='Sports' items={this.state.items} />
                 <CategoryList categoryTitle='Music' items={this.state.items} />
-            </View>
+            </ScrollView>
         );
     }
 }
